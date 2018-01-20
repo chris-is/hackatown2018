80cd67d810c9758ede027a3e86d24f067f4ef0b3
Chris Issa <chris-jorjio.issa@mail.mcgill.ca>
2018-01-20 17:09:07 -0500

    test

--- WebApp/backend/db.php
+++ WebApp/backend/db.php
@@ -9,16 +9,21 @@ $app->post('/posted', function ($request) {
 	require 'database.php';
 	$db = getDB();
 	$message = $request->getParam('message');
+	echo '<script language="javascript">';
+	echo 'alert("message successfully sent")';
+	echo '</script>';
 
 	try {
 		//Add user input to table's message field.
-		$query = "INSERT INTO `client` (`message`,`geolocation`,`time`,`priority`,`status`) VALUES ($message,?,?,?,?)" ;
+		$query = "INSERT INTO `client` (`message`,`geolocation`,`time`,`priority`,`status`) VALUES ($message,?,?,?,?)";
 		$stmt = $db->prepare($query);
 		$stmt->execute();
-		echo "success";
+		echo '<script language="javascript">';
+		echo 'alert("message successfully sent")';
+		echo '</script>';
 	}
 	catch(Exception $e) {
-		echo "Error while updating message"
+		echo "Error while updating message";
 	}
 });
 
