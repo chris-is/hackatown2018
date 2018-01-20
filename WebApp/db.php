51aa62b0dd20a0e83c3a9424cdaf731f9d9e7adc
VU NHU ANH <vunhuanh@gmail.com>
2018-01-20 17:44:24 -0500

    geo

--- WebApp/db.php
+++ WebApp/db.php
@@ -5,21 +5,10 @@ require '../vendor/autoload.php';
 
 $app = new \Slim\App;
 
-$app->post('/posted', function ($request) {
-	require 'database.php';
-	$db = getDB();
-	$message = $request->getParam('message');
 
-	try {
-		//Add user input to table's message field.
-		$query = "INSERT INTO `client` (`message`,`geolocation`,`time`,`priority`,`status`) VALUES ($message,?,?,?,?)" ;
-		$stmt = $db->prepare($query);
-		$stmt->execute();
-		echo "success";
-	}
-	catch(Exception $e) {
-		echo "Error while updating message"
-	}
+
+$app->post('/parse', function ($request) {
+	require 'parse.php';
 });
 
 $app->run();
--- na-codebits/geolocation.js
+++ na-codebits/geolocation.js
@@ -1,33 +1,3 @@
-/*$(document).ready(function(){
-  $('#submit').click(function() {
-    console.log("test");
-    //Check if HTLM5 Geolocation is supported
-    var latitude;
-    var longitude;
-    function showPosition(position) {
-      latitude = position.coords.latitude; 
-      longitude = position.coords.longitude; 
-    }
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-        var timestamp = Date.now();
-        var postdata = "latitude="+latitude+"&longitude="+longitude+"&timestamp"+timestamp;
-        console.log(postdata);
-        $.ajax({
-        type: "POST",
-        url: "http://localhost/hackatown2018/parse.php",
-        data: postdata,
-        success: function(data){
-          console.log(data);
-        }
-      });
-    } else {
-        ('#error-textbox').html = "Geolocation is not supported by this browser.";
-    }
-  });
-
-});*/
-
 $(document).ready(function(){
     $('#submit').click(function() {
       //Check if HTLM5 Geolocation is supported
@@ -42,9 +12,11 @@ $(document).ready(function(){
         var timestamp = Date.now();
         var postdata = "latitude="+latitude+"&longitude="+longitude+"&timestamp"+timestamp;
         console.log(postdata);
+        var base_url="http://localhost/mtlwatch/WebApp/";
+        var url = base_url+'parse';
         $.ajax({
           type: "POST",
-          url: "http://localhost/hackatown2018/na-codebits/parse.php",
+          url: url,
           data: postdata,
           success: function(data){
             console.log(data);
