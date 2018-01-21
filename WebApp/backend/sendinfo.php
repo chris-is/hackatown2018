a7ec4dcf24d49576aa6e0becc6c5cb7b4ef083ea
VU NHU ANH <vunhuanh@gmail.com>
2018-01-21 00:40:41 -0500

    second php part

--- WebApp/backend/sendinfo.php
+++ WebApp/backend/sendinfo.php
@@ -7,7 +7,31 @@
   $match = substr($python, 0, 2);
 
   if($match === "ok"){
+    $service = array(
+      "fire" => "fire department",
+      "robbery" => "police",
+      "terrorist" => "police",
+      "tsunami" => "weather department",
+      "fight" => "abc",
+    );
+
+
     echo "redirected \n";
+    $status = 2;
+
+    //See which issues should be alerted
+    $query = "SELECT keyword from client where status=?";
+    $stmt = $db->prepare($query);
+    $stmt->execute([$status]);
+    $keywords = $stmt->fetchAll();
+
+    //Check which department should be alerted
+    foreach($keywords as $value){
+      $val = $value['keyword'];
+      echo $service[$val];
+    }
+
+
   }
   else{
     echo "no";
